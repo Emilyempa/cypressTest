1492c10569060d2cbf27d57da572907d0c65517a
Emilyempa <emilypettersson@hotmail.com>
2025-02-21 13:50:47 +0100

    add gitignore, cypress.config and packes.json files

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    // baseUrl: 'https://paajt.github.io/ESC-Hacker-Escape-Rooms',
+    setupNodeEvents(on, config) {},
+  },
+});
