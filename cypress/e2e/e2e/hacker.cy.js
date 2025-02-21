1e184ec650ebe3e2731b37c041a549b8636e3c5b
Emilyempa <emilypettersson@hotmail.com>
2025-02-21 16:12:04 +0100

    format page

--- cypress/e2e/e2e/hacker.cy.js
+++ cypress/e2e/e2e/hacker.cy.js
@@ -1,51 +1,51 @@
 /// <reference types="cypress" />
 
 describe("e2e test", () => {
-    it("will check if webpage is running and verifies h1 and hero content", () => {
-      cy.visit("/");
-      cy.get("h2").should("contain.text", "Popular challenges right now");
-      cy.get(".bannerTop").should("be.visible");
-    });
-  
-    it("will klick on link About us and Play online check url and content on both", () => {
-      cy.visit("/");
-      cy.contains("About us").click({ force: true });
-      cy.url().should("include", "/aboutus.html");
-      cy.get("h1").should("contain.text", "About us");
-      cy.get("h3").should("contain.text", "Our history");
-      cy.contains("Play online").click({ force: true });
-      cy.url().should("include", "/challenges.html");
-      cy.get("h1").should("contain.text", "Play online/Play on-site");
-    });
-  
-    it("will test filter and filter content", () => {
-      cy.visit("/challenges.html");
-      cy.contains("Filter challenges").click();
-      cy.get("p").should("contain.text", "By tags");
-      cy.get("p").should("contain.text", "Sort challenges");
-      cy.wait(1000);
-      cy.get("#onlineSortBtn").click();
-      cy.get(".bookBtn").should("contain.text", "Take challenge online");
-    });
-  
-    it("will test search bar in filter", () => {
-      cy.visit("/challenges.html");
-      cy.contains("Filter challenges").click();
-      cy.wait(1000);
-      cy.get("#userInput").click().type("Hacker");
-      cy.get("p").should("contain.text", "Hackers of the world, unite");
-    });
-  
-    it("will test search bar in filter when search fails", () => {
-      cy.visit("/challenges.html");
-      cy.contains("Filter challenges").click();
-      cy.wait(1000);
-      cy.get("#userInput").click().type("ööö");
-      cy.get("h2").should("contain.text", "No matching challenges.");
-    });
-    
-    it("will test if there are 3 Challenges cards on startpage", () => {
-        cy.visit("/");
-        cy.get(".roomIndividual").should("have.length", 3);
-    });
+  it("will check if webpage is running and verifies h1 and hero content", () => {
+    cy.visit("/");
+    cy.get("h2").should("contain.text", "Popular challenges right now");
+    cy.get(".bannerTop").should("be.visible");
   });
+
+  it("will klick on link About us and Play online check url and content on both", () => {
+    cy.visit("/");
+    cy.contains("About us").click({ force: true });
+    cy.url().should("include", "/aboutus.html");
+    cy.get("h1").should("contain.text", "About us");
+    cy.get("h3").should("contain.text", "Our history");
+    cy.contains("Play online").click({ force: true });
+    cy.url().should("include", "/challenges.html");
+    cy.get("h1").should("contain.text", "Play online/Play on-site");
+  });
+
+  it("will test filter and filter content", () => {
+    cy.visit("/challenges.html");
+    cy.contains("Filter challenges").click();
+    cy.get("p").should("contain.text", "By tags");
+    cy.get("p").should("contain.text", "Sort challenges");
+    cy.wait(1000);
+    cy.get("#onlineSortBtn").click();
+    cy.get(".bookBtn").should("contain.text", "Take challenge online");
+  });
+
+  it("will test search bar in filter", () => {
+    cy.visit("/challenges.html");
+    cy.contains("Filter challenges").click();
+    cy.wait(1000);
+    cy.get("#userInput").click().type("Hacker");
+    cy.get("p").should("contain.text", "Hackers of the world, unite");
+  });
+
+  it("will test search bar in filter when search fails", () => {
+    cy.visit("/challenges.html");
+    cy.contains("Filter challenges").click();
+    cy.wait(1000);
+    cy.get("#userInput").click().type("ööö");
+    cy.get("h2").should("contain.text", "No matching challenges.");
+  });
+
+  it("will test if there are 3 Challenges cards on startpage", () => {
+    cy.visit("/");
+    cy.get(".roomIndividual").should("have.length", 3);
+  });
+});
